a2c14e82ac41a062b416f5d337ed06ca7169ffbf
burton.machado@gmail.com <burton.machado@gmail.com>
2018-10-09 20:08:42 -0400

    Adding updates.

--- src/main/kotlin/com/burt/kotlin/functional/Functional_Utils.kt
+++ src/main/kotlin/com/burt/kotlin/functional/Functional_Utils.kt
@@ -1,2 +1,19 @@
 package com.burt.kotlin.functional
 
+data class Person(val firstName: String, val lastName: String, val salary: Double)
+
+fun main(args: Array<String>) {
+    testFunc()
+}
+
+fun testFunc() {
+    val persons = listOf<Person>(Person("Burton", "Machado", 1000000.0), Person("Ruth", "Machado", 2000000.0), Person("Shaun", "Machado", 4000000.0))
+    val filtered = persons.filter { it.salary < 4000000.0 }
+    println("*********** Printing Filtered ***********")
+    filtered.forEach({ println(it) })
+    println("------------------------------------------------")
+    println("*********** Printing Mapped ***********")
+    val mapped = persons.map { it.firstName + " : " + it.lastName }
+    mapped.forEach({ println(it) })
+    println("------------------------------------------------")
+}

ef45b3acdeb1bb80bf326d30c73692cd09dc180d
burton.machado@gmail.com <burton.machado@gmail.com>
2020-03-02 21:09:48 -0500

    OGU

--- src/main/kotlin/functions.kt
+++ src/main/kotlin/functions.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
 
+
+fun myAdd(i:Int, j:Int) = i + j  //return type inferred. Single line function with no body.
+
+fun myAdd1(i:Int, j:Int):Int{
+    return i + j   //Return statement and type required if you use curly brackets to define a function.
+}
+//Default parameters
+fun defParams(i:Int, j:Int=10):Int{
+    return i + j
+}
+
+// Variable number of parameters using vararg.
+fun printStrings(vararg strings:String){
+    for(string in strings){
+        println("string : " + string)
+    }
+}
+
+fun main(args: Array<String>) {
+    println(defParams(2))
+    println(myAdd1(2,3 ))
+    println(myAdd(i=8, j=7)) //Named parameters
 }

d80bfc79397de2ff99ae3f3fdb5337dc28ae7065
machadob <burton.machado@gmail.com>
2020-03-04 19:53:35 -0500

    OGU

---
+++ src/main/kotlin/enums.kt
@@ -0,0 +1,25 @@
+
+enum class COLOR{
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW
+}
+
+enum class COLOR1(val value: Int){
+    RED(-1),
+    GREEN(4),
+    BLUE(6),
+    YELLOW(7)
+}
+
+fun main(args: Array<String>) {
+    val color = COLOR.BLUE
+    val color1 = COLOR1.BLUE
+    println("color : " + color.name)
+    println("color : " + color.ordinal)
+    println("color : " + color)
+    println("color1 : " + color1.name)
+    println("color1 : " + color1.ordinal)
+    println("color1 : " + color1.value)  //6
+}

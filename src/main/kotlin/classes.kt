30aa8535b7dc6e9b3816bb4af5512b74381ea4c7
machadob <burton.machado@gmail.com>
2020-03-04 20:35:07 -0500

    OGU

--- src/main/kotlin/classes.kt
+++ src/main/kotlin/classes.kt
@@ -1,72 +1,102 @@
-
-
 class AirPlane  //A very simple class
 
-class Customer{
+class Customer {
     val id = 7  //This is a immutable property
     var name = "john" //This is a mutable property
 }
 
-class Person(firstName_:String, lastName_:String){
+class Person(firstName_: String, lastName_: String) {
     val firstName = firstName_
     var lastName = lastName_
 }
 
-class NewPerson(val firstName:String, var lastName:String)  //2 properties. one read-only and one read-write.
+class NewPerson(val firstName: String, var lastName: String)  //2 properties. one read-only and one read-write.
 
-class NewPerson1(val firstName:String, var lastName:String){ //2 properties. one read-only and one read-write.
+class NewPerson1(val firstName: String, var lastName: String) { //2 properties. one read-only and one read-write.
     init {
         lastName = lastName.toUpperCase() // Update property in init. init called after construction.
     }
 }
 
-class NewPerson2(val firstName:String, var lastName:String = "John")  //Default arg in constructors.
+class NewPerson2(val firstName: String, var lastName: String = "John")  //Default arg in constructors.
 
-class NewPerson3(val firstName:String, var lastName:String = "John")  {//Primary constructor
-    constructor(firstName:String) : this(firstName,"Joe") { // Secondary constructor. Need to call the super with this.
+class NewPerson3(val firstName: String, var lastName: String = "John") {
+    //Primary constructor
+    constructor(firstName: String) : this(firstName, "Joe") { // Secondary constructor. Need to call the super with this.
 
     }
 }
 
 //Custom getters and setters.
-class NewPerson4(val firstName:String, val offset:Int = 10, var lastName:String = "John"){
-    val age:Int  //Property with a custom getter
-    get() {
-        return offset*7
-    }
-    var ssn:String= ""
-    set(value) { // Custom setter
-        field = java.util.Date().toString() + value  //field is a special value that refers to the underlying property.
-    }
+class NewPerson4(val firstName: String, val offset: Int = 10, var lastName: String = "John") {
+    val age: Int  //Property with a custom getter
+        get() {
+            return offset * 7
+        }
+    var ssn: String = ""
+        set(value) { // Custom setter
+            field = java.util.Date().toString() + value  //field is a special value that refers to the underlying property.
+        }
 }
 
-class NewPerson5(val firstName:String, var lastName:String = "Doe"){ // Functions in a class
-    fun getName():String{ //Default is public access for class members.
+class NewPerson5(val firstName: String, var lastName: String = "Doe") { // Functions in a class
+    fun getName(): String { //Default is public access for class members.
         return firstName + lastName
     }
 
-    private fun getName1():String{ // private function only visible inside the class.
+    private fun getName1(): String { // private function only visible inside the class.
         return firstName + lastName
     }
 
-    protected fun getName2():String{// protected function visible inside the class and it's subclasses.
+    protected fun getName2(): String {// protected function visible inside the class and it's subclasses.
         return firstName + lastName
     }
 }
 
 //Data class
-data class Employee(var firstName:String, val lastName: String, val id:Int, val hobbies:List<String>)
+data class Employee(var firstName: String, val lastName: String, val id: Int, val hobbies: List<String>)
 
 //Data class with custom setter
-data class Employee1(var firstName:String, val lastName: String, val id:Int, val hobbies:List<String>){
-    var ssn:String = "7777"
-        set(value){
-            field="4444"
+data class Employee1(var firstName: String, val lastName: String, val id: Int, val hobbies: List<String>) {
+    var ssn: String = "7777"
+        set(value) {
+            field = "4444"
         }
 
     override fun toString(): String { // You can override any auto generated methods in a data class.classes.kt
         return "Overridden toString() " + super.toString()
     }
+}
+
+open class Animal {
+    // If not declared open, the class is considered final and cannot be subclasses.
+    open fun speak() { // If not declared open, the method is considered final and cannot be subclasses.
+    }
+
+    open fun talk(){
+    }
+}
+
+open class Dog : Animal { //Animal has to be declared open
+    override fun speak(){  //override is mandatory for a overridden method.
+    }
+
+    final override fun talk(){ // to prevent overriding this method in a subclass of Dog use final.
+    }
+    constructor():super(){ //Call super constructor using super.
+    }
+}
+
+class GoldenRetriever: Dog() {
+    override fun speak(){
+    }
+//    override fun talk(){ // Cannot override talk since it is declared final in super class Dog.
+//    }
+}
+
+
+object Region { //Singelton object
+    val region = "US"
 }
 
 fun main(args: Array<String>) {
@@ -78,4 +108,9 @@ fun main(args: Array<String>) {
     println("Name : " + customer.name)
     val np = NewPerson5("John")
     println("Name : " + np.getName())
+
+    val localObject = object {
+        val PI = Math.PI
+    }
+    println("local object : " + localObject.PI)
 }

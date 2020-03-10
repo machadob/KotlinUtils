b2b13a1f741aba478ad29d826c9706ec615c183f
burton.machado@gmail.com <burton.machado@gmail.com>
2020-03-09 21:13:11 -0400

    OGU

--- src/main/kotlin/miscellaneous.kt
+++ src/main/kotlin/miscellaneous.kt
@@ -1,3 +1,6 @@
+import java.lang.Exception
+import java.sql.SQLException
+
 fun main(args: Array<String>) {
 // Type casting
 //if(obj is Employee){  // If you check to see if an object is of a particular type, you do not have to type cast it
@@ -24,6 +27,20 @@ fun main(args: Array<String>) {
     }
 
 // Exceptions
+    // Kotlin does not have checked exceptions.
+    // try blocks are expressions as shown below:
+    val retVal = try{
+        1 // This is the value assigned to retVal if there are no exceptions thrown.
+    }catch(exp: SQLException){
+        2 // This is the value assigned to retVal if in case of a SQLException.
+    }catch(exp: Exception){
+        3// This is the value assigned to retVal if in case of a Exception.
+    }finally {
+        //Close resources etc.
+        4 // This is the value assigned to retVal from finally.
+    }
+    println("retVal : $retVal")
+
 
 // Constants
 

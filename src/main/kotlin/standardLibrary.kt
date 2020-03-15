9e0c7bbd9ff61be1848e8b028815df0a3d0dcf47
burton.machado@gmail.com <burton.machado@gmail.com>
2020-03-14 23:01:48 -0400

    OGU

--- src/main/kotlin/standardLibrary.kt
+++ src/main/kotlin/standardLibrary.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     // Create a immutable list.
     val list = listOf("First", "Second", "Third","Fourth")
@@ -19,13 +21,16 @@ fun main(args: Array<String>) {
 
     // Filter, Map, FlatMap etc.
     data class IdHolder(var id:Int)
-    val fullList = (1..20).toList() // Create list of nummbers.
+    val fullList = ((1..20).shuffled()).toList() // Create list of nummbers.
     println(fullList) // Filter the list.
     val filteredList = fullList.filter { it > 10 }
     println(filteredList)
     val mappedList = filteredList.map { IdHolder(it) } // Map the filtered list to IdHolder objects.
     println(mappedList)
-    val filteredAndMappedList = fullList.filter { it > 10 }.map { IdHolder(it) } // Filter and Map in onde pipeline.
+    val filteredAndMappedList = fullList.filter { it > 10 }.map { IdHolder(it) } // Filter and Map in one pipeline.
     println(filteredAndMappedList)
+    val filteredAndMappedAndSortedList = fullList.filter { it > 10 }.map { IdHolder(it) }.sortedBy { it.id } // Filter, Map, Sort in one pipeline.
+    println(filteredAndMappedAndSortedList)
+
 }
 

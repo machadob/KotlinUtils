303efe7da4efabe0d30ee6fbaef55ec1fe6b6ecc
burton.machado@gmail.com <burton.machado@gmail.com>
2020-03-14 22:56:05 -0400

    OGU

--- src/main/kotlin/standardLibrary.kt
+++ src/main/kotlin/standardLibrary.kt
@@ -18,5 +18,14 @@ fun main(args: Array<String>) {
     map.forEach { k, v ->  println("Key : $k, Value: $v")}
 
     // Filter, Map, FlatMap etc.
+    data class IdHolder(var id:Int)
+    val fullList = (1..20).toList() // Create list of nummbers.
+    println(fullList) // Filter the list.
+    val filteredList = fullList.filter { it > 10 }
+    println(filteredList)
+    val mappedList = filteredList.map { IdHolder(it) } // Map the filtered list to IdHolder objects.
+    println(mappedList)
+    val filteredAndMappedList = fullList.filter { it > 10 }.map { IdHolder(it) } // Filter and Map in onde pipeline.
+    println(filteredAndMappedList)
 }
 
